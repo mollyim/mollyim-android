c5f472c0ea67e85ecea538d6463d5ebbc46f6c1b
Oscar Mira <valldrac@molly.im>
2018-06-20 02:36:42 +0200

    Enable privacy options by default
    * Turn on incognito keyboard
    * Enable screen security
    * Hide name and message in notifications

--- src/org/thoughtcrime/securesms/util/TextSecurePreferences.java
+++ src/org/thoughtcrime/securesms/util/TextSecurePreferences.java
@@ -481,7 +481,7 @@ public class TextSecurePreferences {
   }
 
   public static boolean isIncognitoKeyboardEnabled(Context context) {
-    return getBooleanPreference(context, INCOGNITO_KEYBORAD_PREF, false);
+    return getBooleanPreference(context, INCOGNITO_KEYBORAD_PREF, true);
   }
 
   public static boolean isReadReceiptsEnabled(Context context) {
@@ -586,7 +586,7 @@ public class TextSecurePreferences {
   }
 
   public static NotificationPrivacyPreference getNotificationPrivacy(Context context) {
-    return new NotificationPrivacyPreference(getStringPreference(context, NOTIFICATION_PRIVACY_PREF, "all"));
+    return new NotificationPrivacyPreference(getStringPreference(context, NOTIFICATION_PRIVACY_PREF, "none"));
   }
 
   public static boolean isNewContactsNotificationEnabled(Context context) {
@@ -910,7 +910,7 @@ public class TextSecurePreferences {
   }
 
   public static boolean isScreenSecurityEnabled(Context context) {
-    return getBooleanPreference(context, SCREEN_SECURITY_PREF, false);
+    return getBooleanPreference(context, SCREEN_SECURITY_PREF, true);
   }
 
   public static boolean isLegacyUseLocalApnsEnabled(Context context) {
