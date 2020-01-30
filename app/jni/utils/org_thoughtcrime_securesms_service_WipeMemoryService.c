3836ca98eb6e3bc8e8f6aaed12c32d6811d70d45
Oscar Mira <valldrac@molly.im>
2020-01-31 00:08:36 +0100

    Reformat c code with 2 spaces

--- app/jni/utils/org_thoughtcrime_securesms_service_WipeMemoryService.c
+++ app/jni/utils/org_thoughtcrime_securesms_service_WipeMemoryService.c
@@ -3,40 +3,39 @@
 
 #include "stdlib.h"
 
-#define PAGE_SIZE   sysconf(_SC_PAGE_SIZE)
+#define PAGE_SIZE sysconf(_SC_PAGE_SIZE)
 
 JNIEXPORT jlong JNICALL Java_org_thoughtcrime_securesms_service_WipeMemoryService_allocPages
-        (JNIEnv *env, jclass clazz, jint count)
+    (JNIEnv *env, jclass clazz, jint count)
 {
-    if (count == 0)
-        return (long) NULL;
+  if (count == 0) return (long) NULL;
 
-    void *p = malloc(count * PAGE_SIZE);
+  void *p = malloc(count * PAGE_SIZE);
 
-    return (long) p;
+  return (long) p;
 }
 
 JNIEXPORT void JNICALL Java_org_thoughtcrime_securesms_service_WipeMemoryService_freePages
-        (JNIEnv *env, jclass clazz, jlong p)
+    (JNIEnv *env, jclass clazz, jlong p)
 {
-    free((void *) p);
+  free((void *) p);
 }
 
 JNIEXPORT void JNICALL Java_org_thoughtcrime_securesms_service_WipeMemoryService_wipePage
-        (JNIEnv *env, jclass clazz, jlong p, jint index)
+    (JNIEnv *env, jclass clazz, jlong p, jint index)
 {
-    int *x = (void *) p + (index * PAGE_SIZE);
+  int *x = (void *) p + (index * PAGE_SIZE);
 
-    int size = PAGE_SIZE;
+  int size = PAGE_SIZE;
 
-    do {
-        *x++ = rand();
-        size -= sizeof(*x);
-    } while (size);
+  do {
+    *x++ = rand();
+    size -= sizeof(*x);
+  } while (size);
 }
 
 JNIEXPORT jint JNICALL Java_org_thoughtcrime_securesms_service_WipeMemoryService_getPageSize
-        (JNIEnv *env, jclass clazz)
+    (JNIEnv *env, jclass clazz)
 {
-    return PAGE_SIZE;
+  return PAGE_SIZE;
 }
