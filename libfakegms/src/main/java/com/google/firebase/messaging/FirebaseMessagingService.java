280679ab74682a3d5be93cece4003ab2e5348870
Oscar Mira <valldrac@molly.im>
2020-11-21 01:23:13 +0100

    Move FcmReceiveService to nonFree manifest
    Fixes "non-instantiatable" linter issue.

--- libfakegms/src/main/java/com/google/firebase/messaging/FirebaseMessagingService.java
+++ libfakegms/src/main/java/com/google/firebase/messaging/FirebaseMessagingService.java
@@ -1,6 +1,18 @@
 package com.google.firebase.messaging;
 
-public abstract class FirebaseMessagingService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public abstract class FirebaseMessagingService extends Service {
+  @Nullable
+  @Override
+  public IBinder onBind(Intent intent) {
+    return null;
+  }
+
   public abstract void onMessageReceived(RemoteMessage remoteMessage);
 
   public abstract void onNewToken(String token);
