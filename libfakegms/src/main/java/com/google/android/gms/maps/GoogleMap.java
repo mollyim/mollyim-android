a2f582f94e3b1f7d2f0ac82301f0726ff50c7437
Unknown <johanw@xs4all.nl>
2020-12-03 18:00:49 +0100

    Added Google maptypes to fakegms library

--- libfakegms/src/main/java/com/google/android/gms/maps/GoogleMap.java
+++ libfakegms/src/main/java/com/google/android/gms/maps/GoogleMap.java
@@ -5,7 +5,12 @@ import android.graphics.Bitmap;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GoogleMap {
+  public static final int MAP_TYPE_NONE = 0;
   public static final int MAP_TYPE_NORMAL = 1;
+  public static final int MAP_TYPE_SATELLITE = 2;
+  public static final int MAP_TYPE_HYBRID = 4;
+  public static final int MAP_TYPE_TERRAIN = 3;
+  
 
   public UiSettings getUiSettings() {
     return new UiSettings();
