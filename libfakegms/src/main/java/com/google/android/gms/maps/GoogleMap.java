7f2c26424a652f6afadbb2d82e377d85897c662b
Unknown <johanw@xs4all.nl>
2020-12-10 12:51:41 +0100

    Prevented crash when no Google Maps installed. Improved code layout.

--- libfakegms/src/main/java/com/google/android/gms/maps/GoogleMap.java
+++ libfakegms/src/main/java/com/google/android/gms/maps/GoogleMap.java
@@ -8,8 +8,8 @@ public class GoogleMap {
   public static final int MAP_TYPE_NONE = 0;
   public static final int MAP_TYPE_NORMAL = 1;
   public static final int MAP_TYPE_SATELLITE = 2;
-  public static final int MAP_TYPE_HYBRID = 4;
   public static final int MAP_TYPE_TERRAIN = 3;
+  public static final int MAP_TYPE_HYBRID = 4;
   
 
   public UiSettings getUiSettings() {
