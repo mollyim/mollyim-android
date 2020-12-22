ba9c3ae8ada1d65c8ee25f93b49a64c5fa718048
Oscar Mira <valldrac@molly.im>
2020-12-23 00:24:11 +0100

    Fix crash when inflating libfakegms MapView

--- libfakegms/src/main/java/com/google/android/gms/maps/MapView.java
+++ libfakegms/src/main/java/com/google/android/gms/maps/MapView.java
@@ -2,11 +2,20 @@ package com.google.android.gms.maps;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class MapView extends View {
   public MapView(Context context) {
     super(context);
+  }
+
+  public MapView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public MapView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
   }
 
   public void getMapAsync(OnMapReadyCallback cb) {
