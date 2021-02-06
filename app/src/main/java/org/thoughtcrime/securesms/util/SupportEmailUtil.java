189ae0c5dc985d68afa5f02c4d8dbb6b8a8ad569
Oscar Mira <valldrac@molly.im>
2021-02-06 10:27:08 +0100

    Revert "Add registration lock status to support email."
    This reverts commit 263af7c13993f883337aec0e93f2f50df52f8add.

--- app/src/main/java/org/thoughtcrime/securesms/util/SupportEmailUtil.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/SupportEmailUtil.java
@@ -11,7 +11,6 @@ import androidx.annotation.StringRes;
 import org.signal.core.util.EnglishResourceUtil;
 import org.thoughtcrime.securesms.BuildConfig;
 import org.thoughtcrime.securesms.R;
-import org.thoughtcrime.securesms.keyvalue.SignalStore;
 
 import java.util.Locale;
 
@@ -51,8 +50,6 @@ public final class SupportEmailUtil {
            "\n" +
            context.getString(R.string.SupportEmailUtil_signal_package) + " " + getSignalPackage(context) +
            "\n" +
-           context.getString(R.string.SupportEmailUtil_registration_lock) + " " + getRegistrationLockEnabled(context) +
-           "\n" +
            context.getString(R.string.SupportEmailUtil_locale) + " " + Locale.getDefault().toString();
   }
 
@@ -70,9 +67,5 @@ public final class SupportEmailUtil {
 
   private static CharSequence getSignalPackage(@NonNull Context context) {
     return String.format("%s (%s)", BuildConfig.APPLICATION_ID, AppSignatureUtil.getAppSignature(context).or("Unknown"));
-  }
-
-  private static CharSequence getRegistrationLockEnabled(@NonNull Context context) {
-    return String.valueOf(TextSecurePreferences.isV1RegistrationLockEnabled(context) || SignalStore.kbsValues().isV2RegistrationLockEnabled());
   }
 }
