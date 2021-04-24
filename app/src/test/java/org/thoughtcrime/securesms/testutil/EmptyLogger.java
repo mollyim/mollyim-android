d1700e9296509370ab4514936e643e570305041f
Oscar Mira <valldrac@molly.im>
2021-04-24 20:54:28 +0200

    Fix EmptyLogger

--- app/src/test/java/org/thoughtcrime/securesms/testutil/EmptyLogger.java
+++ app/src/test/java/org/thoughtcrime/securesms/testutil/EmptyLogger.java
@@ -1,4 +1,29 @@
 package org.thoughtcrime.securesms.testutil;
 
-public class EmptyLogger extends org.signal.core.util.logging.EmptyLogger {
+import org.signal.core.util.logging.Log;
+
+public class EmptyLogger extends Log.Logger {
+  @Override
+  public void v(String tag, String message, Throwable t) { }
+
+  @Override
+  public void d(String tag, String message, Throwable t) { }
+
+  @Override
+  public void i(String tag, String message, Throwable t) { }
+
+  @Override
+  public void w(String tag, String message, Throwable t) { }
+
+  @Override
+  public void e(String tag, String message, Throwable t) { }
+
+  @Override
+  public void wtf(String tag, String message, Throwable t) { }
+
+  @Override
+  public void blockUntilAllWritesFinished() { }
+
+  @Override
+  public void clear() { }
 }
