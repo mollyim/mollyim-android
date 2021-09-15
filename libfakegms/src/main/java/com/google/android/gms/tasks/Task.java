2066356e6d519969f48192cf64252af7d7fb1907
Oscar Mira <valldrac@molly.im>
2021-09-15 12:03:18 +0200

    Add missing stubs to libfakegms

--- libfakegms/src/main/java/com/google/android/gms/tasks/Task.java
+++ libfakegms/src/main/java/com/google/android/gms/tasks/Task.java
@@ -1,16 +1,29 @@
 package com.google.android.gms.tasks;
 
 public class Task<TResult> {
+  public boolean isSuccessful() {
+    return false;
+  }
+
+  public TResult getResult() {
+    return null;
+  }
+
+  public Exception getException() {
+    return new UnsupportedOperationException();
+  }
+
   public Task<TResult> addOnSuccessListener(OnSuccessListener<TResult> listener) {
     return this;
   }
 
   public Task<TResult> addOnFailureListener(OnFailureListener<TResult> listener) {
-    listener.onFailure(new UnsupportedOperationException());
+    listener.onFailure(getException());
     return this;
   }
 
-  public TResult getResult() {
-    return null;
+  public Task<TResult> addOnCompleteListener(OnCompleteListener<TResult> listener) {
+    listener.onComplete(this);
+    return this;
   }
 }
