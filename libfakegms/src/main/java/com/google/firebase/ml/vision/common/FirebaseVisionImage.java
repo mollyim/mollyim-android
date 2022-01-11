5b2c8ddef8a784b07ba8358813ea668edc5987d8
Oscar Mira <valldrac@molly.im>
2022-01-11 11:34:52 +0100

    Remove unused Firebase ML vision stubs

--- libfakegms/src/main/java/com/google/firebase/ml/vision/common/FirebaseVisionImage.java
+++
@@ -1,9 +0,0 @@
-package com.google.firebase.ml.vision.common;
-
-import android.graphics.Bitmap;
-
-public class FirebaseVisionImage {
-  public static FirebaseVisionImage fromBitmap(Bitmap source) {
-    return new FirebaseVisionImage();
-  }
-}
--- libfakegms/src/main/java/com/google/firebase/ml/vision/face/FirebaseVisionFace.java
+++
@@ -1,9 +0,0 @@
-package com.google.firebase.ml.vision.face;
-
-import android.graphics.Rect;
-
-public class FirebaseVisionFace {
-  public static Rect getBoundingBox(FirebaseVisionFace firebaseVisionFace) {
-    return new Rect();
-  }
-}
