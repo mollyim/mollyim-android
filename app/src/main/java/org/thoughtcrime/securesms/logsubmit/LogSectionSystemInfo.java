647ab61eb735769d64494f49759fc2db78e9d70b
Oscar Mira <valldrac@molly.im>
2022-05-13 20:22:54 +0200

    Do not initialize telecom in debug log while locked

--- app/src/main/java/org/thoughtcrime/securesms/logsubmit/LogSectionSystemInfo.java
+++ app/src/main/java/org/thoughtcrime/securesms/logsubmit/LogSectionSystemInfo.java
@@ -80,7 +80,7 @@ public class LogSectionSystemInfo implements LogSection {
     builder.append("Days Installed: ").append(VersionTracker.getDaysSinceFirstInstalled(context)).append("\n");
     builder.append("Build Variant : ").append(BuildConfig.FLAVOR_distribution).append(BuildConfig.BUILD_ENVIRONMENT_TYPE).append(BuildConfig.BUILD_VARIANT_TYPE).append("\n");
     builder.append("Emoji Version : ").append(getEmojiVersionString(context)).append("\n");
-    builder.append("Telecom       : ").append(AndroidTelecomUtil.getTelecomSupported()).append("\n");
+    builder.append("Telecom       : ").append(locked ? "Unknown" : AndroidTelecomUtil.getTelecomSupported()).append("\n");
     builder.append("User-Agent    : ").append(StandardUserAgentInterceptor.USER_AGENT).append("\n");
     builder.append("App           : ");
     try {
