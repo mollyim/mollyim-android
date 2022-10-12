66119e9f30b4a6abbc2c35366a0a7a8d2ad41096
Oscar Mira <valldrac@molly.im>
2022-10-12 10:26:03 +0200

    Reinitialize IncomingMessageObserver on network connections restart

--- app/src/main/java/org/thoughtcrime/securesms/dependencies/ApplicationDependencies.java
+++ app/src/main/java/org/thoughtcrime/securesms/dependencies/ApplicationDependencies.java
@@ -278,6 +278,7 @@ public class ApplicationDependencies {
         signalWebSocket.forceNewWebSockets();
       }
     }
+    getIncomingMessageObserver();
   }
 
   public static @NonNull SignalServiceNetworkAccess getSignalServiceNetworkAccess() {
