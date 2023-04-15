24392550652543d5f1c001c01cbc65b619227fc6
h3ph4est7s <h3ph4est7s@users.noreply.github.com>
2023-04-15 18:19:38 +0300

    Increment minimum supported android version to Nougat (API 24)

--- build-logic/plugins/src/main/java/android-constants.gradle.kts
+++ build-logic/plugins/src/main/java/android-constants.gradle.kts
@@ -2,5 +2,5 @@
 val signalBuildToolsVersion by extra("32.0.0")
 val signalCompileSdkVersion by extra("android-33")
 val signalTargetSdkVersion by extra(31)
-val signalMinSdkVersion by extra(23)
+val signalMinSdkVersion by extra(24)
 val signalJavaVersion by extra(JavaVersion.VERSION_11)
