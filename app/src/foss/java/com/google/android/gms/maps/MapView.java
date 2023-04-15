61f34073147160adf9e51b134c3f522c1c3eb023
h3ph4est7s <h3ph4est7s@users.noreply.github.com>
2023-04-15 18:28:39 +0300

    Make osmdroid proxy aware

--- app/src/foss/java/com/google/android/gms/maps/MapView.java
+++ app/src/foss/java/com/google/android/gms/maps/MapView.java
@@ -17,6 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.config.IConfigurationProvider;
 import org.osmdroid.tileprovider.TileStates;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsDisplay;
@@ -25,6 +26,9 @@ import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 import org.thoughtcrime.securesms.BuildConfig;
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.net.Network;
+
+import java.net.Proxy;
 
 public class MapView extends org.osmdroid.views.MapView {
   private Marker               marker;
@@ -53,8 +57,13 @@ public class MapView extends org.osmdroid.views.MapView {
 
   private void setDefaultConfiguration(Context context) {
     String userAgent = String.format("%s/%s", context.getString(R.string.app_name), BuildConfig.VERSION_NAME);
-    Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
-    Configuration.getInstance().setUserAgentValue(userAgent);
+    final IConfigurationProvider config = Configuration.getInstance();
+    final Proxy proxy = Network.getProxy();
+    config.load(context, PreferenceManager.getDefaultSharedPreferences(context));
+    config.setUserAgentValue(userAgent);
+    if (proxy != null) {
+      config.setHttpProxy(proxy);
+    }
 
     getZoomController().getDisplay().setPositions(
         false,
