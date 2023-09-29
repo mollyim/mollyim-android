bfbaae21e49097373be478325524280d1ea24055
Oscar Mira <valldrac@molly.im>
2023-09-29 11:07:58 +0200

    Extract WebP decoder to an external repo

--- glide-webp/lib/build.gradle.kts
+++ glide-webp/lib/build.gradle.kts
@@ -10,27 +10,12 @@ plugins {
 
 android {
   namespace = "org.signal.glide.webp"
-
-  defaultConfig {
-    externalNativeBuild {
-      cmake {
-        cppFlags("-std=c++17", "-fvisibility=hidden")
-        arguments("-DLIBWEBP_PATH=$rootDir/libwebp")
-      }
-    }
-  }
-
-  externalNativeBuild {
-    cmake {
-      path = file("$projectDir/src/main/cpp/CMakeLists.txt")
-      version = "3.22.1"
-    }
-  }
 }
 
 dependencies {
   implementation(project(":core-util"))
 
   implementation(libs.glide.glide)
+  implementation(libs.molly.glide.webp.decoder)
   kapt(libs.glide.compiler)
 }
