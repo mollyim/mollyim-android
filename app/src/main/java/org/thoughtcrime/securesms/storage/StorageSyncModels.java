mollyim/mollyim-android

commit 7689a81dc8b8164120cd19b8f3a9d1fa08989d72

Sync pinned chats for both registered and unregistered contacts

app/src/main/java/org/thoughtcrime/securesms/storage/StorageSyncModels.java
@@ -85,8 +85,7 @@ public final class StorageSyncModels {
   public static List<SignalAccountRecord.PinnedConversation> localToRemotePinnedConversations(@NonNull List<RecipientRecord> settings) {
     return Stream.of(settings)
                  .filter(s -> s.getRecipientType() == RecipientTable.RecipientType.GV1 ||
-                              s.getRecipientType() == RecipientTable.RecipientType.GV2 ||
-                              s.getRegistered() == RecipientTable.RegisteredState.REGISTERED)
+                              s.getRecipientType() == RecipientTable.RecipientType.GV2)
                  .map(StorageSyncModels::localToRemotePinnedConversation)
                  .toList();
   }

commit db0802ab869b06167e2851e572505c6e92d25620

Fix issue with filtering when syncing pinned chats
Bug introduced by commit 7689a81dc8b8164120cd19b8f3a9d1fa08989d72.

Fixes #264, fixes #266

app/src/main/java/org/thoughtcrime/securesms/storage/StorageSyncModels.java
@@ -84,7 +84,10 @@ public final class StorageSyncModels {
   public static List<SignalAccountRecord.PinnedConversation> localToRemotePinnedConversations(@NonNull List<RecipientRecord> settings) {
     return Stream.of(settings)
                  .filter(s -> s.getRecipientType() == RecipientTable.RecipientType.GV1 ||
-                              s.getRecipientType() == RecipientTable.RecipientType.GV2)
+                              s.getRecipientType() == RecipientTable.RecipientType.GV2 ||
+                              s.getRegistered() == RecipientTable.RegisteredState.REGISTERED ||
+                              s.getRegistered() == RecipientTable.RegisteredState.NOT_REGISTERED
+                 )
                  .map(StorageSyncModels::localToRemotePinnedConversation)
                  .toList();
   }