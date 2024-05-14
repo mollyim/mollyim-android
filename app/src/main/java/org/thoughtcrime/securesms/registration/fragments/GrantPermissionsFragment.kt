efec84a946488e58b4f19a42884b68fd97858656
materialred <carloschmidtolufsen@duck.com>
2024-05-14 15:14:39 +0200

    Repair device Ragistration

--- app/src/main/java/org/thoughtcrime/securesms/registration/fragments/GrantPermissionsFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/registration/fragments/GrantPermissionsFragment.kt
@@ -6,10 +6,31 @@
 package org.thoughtcrime.securesms.registration.fragments
 
 import android.os.Build
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import org.signal.core.ui.Buttons
@@ -18,7 +39,6 @@ import org.signal.core.util.sp
 import org.thoughtcrime.securesms.R
 import org.thoughtcrime.securesms.compose.ComposeFragment
 import org.thoughtcrime.securesms.permissions.Permissions
-import org.thoughtcrime.securesms.registration.compose.GrantPermissionsScreen
 import org.thoughtcrime.securesms.registration.viewmodel.RegistrationViewModel
 import org.thoughtcrime.securesms.util.BackupUtil
 
@@ -238,7 +258,6 @@ fun NextButton(
       text = stringResource(id = R.string.GrantPermissionsFragment__next),
       style = MaterialTheme.typography.labelSmall,
 
-    )
+      )
   }
 }
-
