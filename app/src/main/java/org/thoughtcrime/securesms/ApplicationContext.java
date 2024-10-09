thebiblelover7/mollyim-android-unifiedpush

Properly restart IncomingMessageObserver Foreground Service
If needed : when switching notification method or when
UnifiedPush status changes.
Always use UnifiedPushRefreshJob to update UP related
component and to register to MollySocket.

app/src/main/java/org/thoughtcrime/securesms/ApplicationContext.java
@@ -464,16 +464,19 @@ public class ApplicationContext extends MultiDexApplication implements AppForegr
     }
   }
 
+  // MOLLY: this initialize FCM, websocket and UnifiedPush
   public void initializeFcmCheck() {
     if (!SignalStore.account().isRegistered()) {
       return;
     }
 
     PlayServicesUtil.PlayServicesStatus fcmStatus = PlayServicesUtil.getPlayServicesStatus(this);
+    boolean unifiedPushAvailable                  = UnifiedPushHelper.isUnifiedPushAvailable();
+    boolean forceWebSocket                        = SignalStore.internalValues().isWebsocketModeForced();
 
-    if (UnifiedPushHelper.isUnifiedPushAvailable()
+    if (unifiedPushAvailable || forceWebSocket
         || fcmStatus == PlayServicesUtil.PlayServicesStatus.DISABLED) {
-      if (!SignalStore.unifiedpush().getAirGaped()) {
+      if (unifiedPushAvailable && !SignalStore.unifiedpush().getAirGaped()) {
         ApplicationDependencies.getJobManager().add(new UnifiedPushRefreshJob());
       }
       ApplicationDependencies.getJobManager().cancel(new FcmRefreshJob().getId());
@@ -491,12 +494,10 @@ public class ApplicationContext extends MultiDexApplication implements AppForegr
                SignalStore.account().getFcmTokenLastSetTime() < 0) {
       Log.i(TAG, "Play Services are newly-available. Updating to use FCM.");
       SignalStore.account().setFcmEnabled(true);
-      ApplicationDependencies.getJobManager().cancel(new UnifiedPushRefreshJob().getId());
       ApplicationDependencies.getJobManager().startChain(new FcmRefreshJob())
                                              .then(new RefreshAttributesJob())
                                              .enqueue();
     } else {
-      ApplicationDependencies.getJobManager().cancel(new UnifiedPushRefreshJob().getId());
       long nextSetTime = SignalStore.account().getFcmTokenLastSetTime() + TimeUnit.HOURS.toMillis(6);
 
       if (SignalStore.account().getFcmToken() == null || nextSetTime <= System.currentTimeMillis()) {

mollyim/mollyim-android

app/src/main/java/org/thoughtcrime/securesms/ApplicationContext.java
@@ -497,16 +497,19 @@ public class ApplicationContext extends Application implements AppForegroundObse
     }
   }
 
+  // MOLLY: this initialize FCM, websocket and UnifiedPush
   public void initializeFcmCheck() {
     if (!SignalStore.account().isRegistered()) {
       return;
     }
 
     PlayServicesUtil.PlayServicesStatus fcmStatus = PlayServicesUtil.getPlayServicesStatus(this);
+    boolean unifiedPushAvailable                  = UnifiedPushHelper.isUnifiedPushAvailable();
+    boolean forceWebSocket                        = SignalStore.internal().isWebsocketModeForced();
 
-    if (UnifiedPushHelper.isUnifiedPushAvailable()
+    if (unifiedPushAvailable || forceWebSocket
         || fcmStatus == PlayServicesUtil.PlayServicesStatus.DISABLED) {
-      if (!SignalStore.unifiedpush().getAirGaped()) {
+      if (unifiedPushAvailable && !SignalStore.unifiedpush().getAirGaped()) {
         AppDependencies.getJobManager().add(new UnifiedPushRefreshJob());
       }
       AppDependencies.getJobManager().cancel(new FcmRefreshJob().getId());
@@ -524,12 +527,10 @@ public class ApplicationContext extends Application implements AppForegroundObse
                SignalStore.account().getFcmTokenLastSetTime() < 0) {
       Log.i(TAG, "Play Services are newly-available. Updating to use FCM.");
       SignalStore.account().setFcmEnabled(true);
-      AppDependencies.getJobManager().cancel(new UnifiedPushRefreshJob().getId());
       AppDependencies.getJobManager().startChain(new FcmRefreshJob())
                                              .then(new RefreshAttributesJob())
                                              .enqueue();
     } else {
-      AppDependencies.getJobManager().cancel(new UnifiedPushRefreshJob().getId());
       long lastSetTime = SignalStore.account().getFcmTokenLastSetTime();
       long nextSetTime = lastSetTime + TimeUnit.HOURS.toMillis(6);
       long now         = System.currentTimeMillis();