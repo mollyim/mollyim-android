6a264e04074f323f59a6026a08786b9878b41dbc
Oscar Mira <valldrac@molly.im>
2024-12-24 11:31:33 +0100

    Add launcher alt icons
    Fixes #233

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/appearance/appicon/AppIconSelectionFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/appearance/appicon/AppIconSelectionFragment.kt
@@ -6,6 +6,7 @@
 package org.thoughtcrime.securesms.components.settings.app.appearance.appicon
 
 import android.content.Context
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -36,7 +37,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -45,6 +49,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.findNavController
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
@@ -255,11 +260,14 @@ fun AppIcon(preset: AppIconPreset, isSelected: Boolean, onClick: () -> Unit, mod
       clip = true
     )
     .clickable(onClick = onClick)
-  Image(
-    painterResource(id = preset.iconPreviewResId),
-    contentDescription = stringResource(id = preset.labelResId),
-    modifier = imageModifier
-  )
+  val adaptiveIcon = AppCompatResources.getDrawable(LocalContext.current, preset.iconPreviewResId)
+  if (adaptiveIcon != null) {
+    Image(
+      BitmapPainter(adaptiveIcon.toBitmap().asImageBitmap()),
+      contentDescription = stringResource(id = preset.labelResId),
+      modifier = imageModifier
+    )
+  }
 }
 
 /**
