1482aaec08c349fbea6cd435675ad6f74ebd9c56
Oscar Mira <valldrac@molly.im>
2025-02-18 12:41:30 +0100

    Restrict Google maven repository scope in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,13 @@ pluginManagement {
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
-    google()
+    google {
+      content {
+        includeGroupByRegex("com\\.android(\\..*)?")
+        includeGroupByRegex("com\\.google(\\..*)?")
+        includeGroupByRegex("androidx?(\\..*)?")
+      }
+    }
     mavenCentral()
     mavenLocal {
       content {
