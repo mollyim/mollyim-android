0709c7fc7bb407bf8a3f18e339b3918363279f31
Oscar Mira <valldrac@molly.im>
2025-03-07 20:46:54 +0100

    Fix app not restarting after package update
    Fixes #421

--- app/src/main/java/org/thoughtcrime/securesms/service/BootReceiver.java
+++ app/src/main/java/org/thoughtcrime/securesms/service/BootReceiver.java
@@ -3,13 +3,17 @@ package org.thoughtcrime.securesms.service;
 import android.content.Context;
 import android.content.Intent;
 
+import org.signal.core.util.logging.Log;
 import org.thoughtcrime.securesms.dependencies.AppDependencies;
 import org.thoughtcrime.securesms.jobs.MessageFetchJob;
 
 public class BootReceiver extends ExportedBroadcastReceiver {
 
+  private static final String TAG = Log.tag(BootReceiver.class);
+
   @Override
   public void onReceiveUnlock(Context context, Intent intent) {
+    Log.i(TAG, "Restarting after: " + intent.getAction());
     AppDependencies.getJobManager().add(new MessageFetchJob());
   }
 }
