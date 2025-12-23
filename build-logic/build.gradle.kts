7f8a0549514d84ce94341ad55e686c8ecbef0c7c
Oscar Mira <valldrac@molly.im>
2025-12-23 14:29:20 +0100

    Revert accidental removal of buildscript repos

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -2,5 +2,12 @@ plugins {
   alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
 
+buildscript {
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
 apply(from = "${rootDir}/../constants.gradle.kts")
 
