837598657dab7a01874ea515e319e72d669dc77e
Michelle Tang <mtang@signal.org>
2026-06-29 12:02:37 -0400

    Bump version to 8.17.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ plugins {
 val staticIps = Properties().apply { file("static-ips.properties").reader().use { load(it) } }
 staticIps.stringPropertyNames().forEach { rootProject.extra[it] = staticIps.getProperty(it) }
 
-val canonicalVersionCode = 1712
-val canonicalVersionName = "8.17.2"
+val canonicalVersionCode = 1713
+val canonicalVersionName = "8.17.3"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
