5a6b0cbfefcab913d152b2bb68dc65d835bd73a4
Alex Hart <alex@signal.org>
2026-07-06 13:35:58 -0300

    Bump version to 8.18.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ plugins {
 val staticIps = Properties().apply { file("static-ips.properties").reader().use { load(it) } }
 staticIps.stringPropertyNames().forEach { rootProject.extra[it] = staticIps.getProperty(it) }
 
-val canonicalVersionCode = 1715
-val canonicalVersionName = "8.18.1"
+val canonicalVersionCode = 1716
+val canonicalVersionName = "8.18.2"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
